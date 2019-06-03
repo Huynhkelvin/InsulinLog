abad363f07662bc9232ab088b9c592d640eeab27
unknown <huynhkelvin90@gmail.com>
2019-06-02 23:21:50 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 insulinlog-project
+"# InsulinLog" 
--- logs/views.py
+++ logs/views.py
@@ -5,15 +5,6 @@ from django.contrib.auth.models import User
 from accounts.forms import ExtendedUserCreationForm
 
 
-    # form = ExtendedUserCreationForm(request.POST)
-    # patient_form = PatientForm(request.POST)
-    #
-    # if form.is_valid() and patient_form.is_valid():
-    #     user = form.save()
-    #     patient = patient_form.save(commit=False)
-    #     patient.user = user
-    #     patient.save()
-
 def insulinform(request):
     if request.method == 'POST':
         form = DailyInsulinForm(request.POST)
@@ -21,13 +12,14 @@ def insulinform(request):
         if form.is_valid():
             logmodel = form.save(commit=False)
             logmodel.patient = request.user
-            print(logmodel.patient)
-            logmodel.diff_BSL = logmodel.curr_BSL-130
-            logmodel.carb_ratio = logmodel.patient.patient.carb_ratio
+            logmodel.diff_BSL = logmodel.curr_BSL - 130
             logmodel.correction_insulin = logmodel.diff_BSL / logmodel.patient.patient.insulin_sens
+            logmodel.carb_ratio = logmodel.patient.patient.carb_ratio
             logmodel.insulin_dose = logmodel.total_carb / logmodel.carb_ratio
-            logmodel.total_insulin = logmodel.insulin_dose + logmodel.correction_insulin
+            logmodel.total_insulin = logmodel.correction_insulin + logmodel.insulin_dose
+
             logmodel.save()
+            print(logmodel)
 
         else:
             print('invalid')
